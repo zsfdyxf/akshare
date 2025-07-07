00160945262e5c3e97036eee32c2dec4fa649b91
hw <haonan629@aliyun.com>
2025-07-07 17:22:06 +0800

    tj

--- tests/test_func_2.py
+++ tests/test_func_2.py
@@ -3,19 +3,26 @@
 import akshare as ak
 import pandas as pd
 
-# 获取数据
-df = ak.fund_etf_hist_em(symbol="518880", period="daily")
 
-# 处理日期列（根据实际列名调整）
-df["日期"] = pd.to_datetime(df["日期"])
 
-# 筛选时间段
-filtered_df = df[(df["日期"] >= "2025-01-01") & (df["日期"] <= "2025-06-30")]
+# 获取全球最大黄金ETF-SPDR持仓数据
+# df = ak.macro_china_fx_gold()
+df = ak.macro_cons_gold()
+print(df)  # 筛选SPDR黄金ETF
 
-# 保存到CSV（可选）
-filtered_df.to_csv("gold_etf_20250101-0630.csv", index=False)
+# # 获取数据
+# df = ak.fund_etf_hist_em(symbol="518880", period="daily")
 
-print(filtered_df)
+# # 处理日期列（根据实际列名调整）
+# df["日期"] = pd.to_datetime(df["日期"])
+
+# # 筛选时间段
+# filtered_df = df[(df["日期"] >= "2025-01-01") & (df["日期"] <= "2025-06-30")]
+
+# # 保存到CSV（可选）
+# filtered_df.to_csv("gold_etf_20250101-0630.csv", index=False)
+
+# print(filtered_df)
 
 # stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="518880", period="daily", start_date="20250601", end_date='20250630', adjust="")
 
